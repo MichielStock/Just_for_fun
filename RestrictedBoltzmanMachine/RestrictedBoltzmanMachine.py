ef4ecc8583379752bb7f31a4a3181ec01c0e3e3f
MichielStock <michiel.stock@telenet.be>
2015-12-23 14:16:01 +0100

    Hinton training implemented

--- RestrictedBoltzmanMachine/RestrictedBoltzmanMachine.py
+++ RestrictedBoltzmanMachine/RestrictedBoltzmanMachine.py
@@ -20,9 +20,9 @@ class RestrictedBoltzmanMachine:
     """
 
     def __init__(self, n_visible, n_hidden):
-        self._bias_visible = np.random.randn(n_visible, 1) / 10
-        self._bias_hidden = np.random.randn(n_hidden, 1) / 10
-        self._weights = np.random.randn(n_visible, n_hidden) / 10
+        self._bias_visible = np.random.randn(n_visible, 1) / 100
+        self._bias_hidden = np.random.randn(n_hidden, 1) / 100
+        self._weights = np.random.randn(n_visible, n_hidden) / 100
         self.n_visible = n_visible
         self.n_hidden = n_hidden
 
@@ -75,7 +75,7 @@ class RestrictedBoltzmanMachine:
                                     - fantasy_hid.sum(0)).reshape((-1,1))
     
     def train_stochastic_gradient_ascent(self, X, learning_rate=0.01,
-                                         sampling_steps=2, iterations=1000,
+                                         sampling_steps=1, iterations=1000,
                                          minibatch_size=10):
         n_instances = len(X)
         instances = range(n_instances)
@@ -91,23 +91,66 @@ class RestrictedBoltzmanMachine:
                 vis_reconstr, _ = self.sample_fantasies(minibatch, 1)
                 mse_reconstr.append(np.mean((vis_reconstr - minibatch)**2))
         return mse_reconstr
-
-
+        
+    def train_C1(self, X, learning_rate=0.01, iterations=1000,
+                 minibatch_size=10, momentum=0.9):
+        """
+        Trains the restricted boltzman machine using Hinton's recommended
+        method making use of a single Gibbs step.
+        Uses momentum to speed up learning
+        """
+        n_instances = len(X)
+        instances = range(n_instances)
+        mse_reconstr = []
+        dW = 0
+        da = 0
+        db = 0
+        for iteration in range(iterations):
+            shuffle(instances)
+            start = 0
+            while start < n_instances:
+                visible = X[start:start+minibatch_size]
+                lr = learning_rate / visible.shape[0]  # correct for sample
+                # size
+                # sample corresponding hidden
+                hidden = self.hidden_given_visible(visible)
+                # get probabilities visible
+                reconstr = self.visible_given_hidden(hidden, sample=False)
+                hidden_CD2 = self.hidden_given_visible(reconstr, sample=False)
+                # new directions
+                dW = (1 - momentum) * lr * (np.dot(visible.T, hidden) -\
+                        np.dot(reconstr.T, hidden)) + momentum * dW
+                da = (1 - momentum) * lr * (visible.sum(0) -\
+                        reconstr.sum(0)).reshape(-1,1) + momentum * da
+                db = (1 - momentum) * lr * (hidden.sum(0) -\
+                        hidden_CD2.sum(0)).reshape(-1,1) + momentum * db
+                # update weights
+                self._weights += dW
+                self._bias_visible += da
+                self._bias_hidden += db
+                # update start
+                start += minibatch_size
+                # error
+                mse_reconstr.append(np.mean((reconstr - visible)**2))
+        return mse_reconstr
+        
+        
 if __name__ == '__main__':
 
     rbm = RestrictedBoltzmanMachine(100, 10)
     pattern = np.kron(np.random.binomial(1, 0.5, (100, 10)), np.ones((1, 10)))
-
-    error = rbm.train_stochastic_gradient_ascent(pattern, learning_rate=0.1,
-                                                 iterations=5000,
-                                                 sampling_steps=2)
+    error = rbm.train_C1(pattern, learning_rate=0.001, iterations=1000)
+    
+    #error = rbm.train_stochastic_gradient_ascent(pattern, learning_rate=0.1,
+   #                                             iterations=5000,
+    #                                             sampling_steps=1)
                                                  
     print rbm.sample_fantasies(np.random.binomial(1, 0.5,(5, 100)))
     """
     error += rbm.train_stochastic_gradient_ascent(pattern, learning_rate=0.01,
                                                  iterations=5000,
-                                                 sampling_steps=4)
+                                                 sampling_steps=1)
     error += rbm.train_stochastic_gradient_ascent(pattern, learning_rate=0.001,
                                                  iterations=5000,
-                                                 sampling_steps=10)
+                                                 sampling_steps=1)
     """
