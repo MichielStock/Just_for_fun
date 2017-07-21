582b5d023edff6a6e3b7d27b29cf0092c899c0f0
MichielStock <michiel.stock@telenet.be>
2017-07-21 15:07:42 +0200

    Generated fingerprints and images of the molecules!

---
+++ Urine_metabolomics/Scripts/generate_fingerprints.py
@@ -0,0 +1,10 @@
+"""
+Created on Friday 21 July 2017
+Last update: -
+
+@author: Michiel Stock
+michielfmstock@gmail.com
+
+Usines chemoinformatics to generate fingerprints and images
+of the urine metabolites
+"""
--- Urine_metabolomics/Scripts/parse_urine_metabolomics_data.py
+++ Urine_metabolomics/Scripts/parse_urine_metabolomics_data.py
@@ -1,6 +1,6 @@
 """
 Created on Thursday 20 July 2017
-Last update: -
+Last update: Friday 21 July 2017
 
 @author: Michiel Stock
 michielfmstock@gmail.com
@@ -10,6 +10,19 @@ Database (http://www.hmdb.ca/).
 """
 
 import xmltodict
+import json
+
+def unlist(potential_list):
+    """
+    Turns possible nested list in a list
+    """
+    real_list = []
+    for el in list(potential_list):
+        if type(el) is list:
+            real_list += el
+        else:
+            real_list.append(el)
+    return real_list
 
 # load the metabolites dataset as a dictonary
 with open('Data/urine_metabolites.xml') as fh:
@@ -18,7 +31,7 @@ with open('Data/urine_metabolites.xml') as fh:
 # clean the dataset in a nice format
 urine_metabolites_data = {}
 
-# simple propeties to collect:
+# simple properties to collect:
 simple_fields = set([
     'description',
     'chemical_formula',
@@ -26,12 +39,41 @@ simple_fields = set([
     'average_molecular_weight'])
 
 for metabolite in urine_metabolites_db['metabolite']:
-    propeties = {}
+    metabolite_data = {}
+    # get name
     name = metabolite['name']
-    urine_metabolites_data[name] = propeties
+    # simple properties
     for k, v in metabolite.items():
         if k in simple_fields:
-            propeties[k] = v
+            metabolite_data[k] = v
+    # chemical taxonomy of the metabolite
     if 'taxonomy' in metabolite:
-        for k in ['kingdom', 'class', 'sub_class']
-        urine_metabolites_data[k] = metabolite[k]
+        for k in ['kingdom', 'class', 'sub_class']:
+            metabolite_data[k] = metabolite['taxonomy'][k]
+            # some ontological information
+    if 'ontology' in metabolite:
+        biofunctions = metabolite['ontology']['biofunctions']
+        if biofunctions is not None:
+            metabolite_data['biofunctions'] = list(set(unlist(biofunctions.values())))
+    # pathways
+    if metabolite['pathways'] is not None:
+        pathways = []
+        for pw_dict in unlist(metabolite['pathways'].values()):
+            pathways.append(pw_dict['name'])
+        metabolite_data['pathways'] = pathways
+    # diseases
+    if metabolite['diseases'] is not None:
+        diseases = []
+        for dis_dict in unlist(metabolite['diseases'].values()):
+            diseases.append(dis_dict['name'])
+        metabolite_data['diseases'] = diseases
+
+    urine_metabolites_data[name] = metabolite_data
+    # normal concentrations
+    if metabolite['normal_concentations'] is not None:
+        
+
+# save the dataset
+with open('Data/urine_metabolome.json', 'w') as fp:
+    json.dump(obj=urine_metabolites_data, fp=fp,
+        indent=True, ensure_ascii=True)
